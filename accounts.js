ba38bce668c28171b19e02a6f83eaae2466cce89
rickid0 <luruicheng@me.com>
2024-03-06 15:10:40 +0800

    feat:实现注册功能

--- accounts.js
+++ accounts.js
@@ -11,36 +11,68 @@ function addLoginListener(app,users){
         users.findOne( {uuid:uuid})
             .then((user)=>{
             if(!user){
-                res.statusCode=404
                 console.log('user not exist')
-                res.end('failed')
+                res.status(404).end('failed')
                 return
             }
-            const realpassword=user.password
+            const realpassword=user.password;
+                console.log(realpassword===password)
             if(realpassword===password){
-                res.statusCode=200
-                res.end('success')
+                res.status(200).end('success')
             }else{
-                res.statusCode=401
-                res.end('success')
+                res.status(401).end('success')
             }
         }).catch(err=>{
             if(err){
                 console.log('find error',err)
-                res.statusCode=500
-                res.end('failed')
+                res.status(500).end('failed')
                 return
             }
         })
     })
+}
+function addRegisterListener(app,users,nextAssignedIdCollection){
+    app.use(express.json());
+    app.post('/register',(req,res)=>{
+        const data=req.body
+        const {password} = data
+        let nextAssignedId
+        nextAssignedIdCollection.findOne({})
+            .then(nextAssignedIdDoc=>{
+                nextAssignedId = nextAssignedIdDoc.value
+                console.log(nextAssignedId)
+
+                users.insertOne({uuid:nextAssignedId.toString(),password:password})
+                    .then(user=>{
+
+                        nextAssignedIdCollection.updateOne(
+                            {value: nextAssignedId},
+                            {$set:{value: nextAssignedId+1}}
+                        ).catch(err=>console.log('update err'))
+
+                        res.status(200).send(nextAssignedId.toString())
+
+                    })
+                    .catch(err=>console.log('insert error'))
+
+            })
+            .catch(err=>console.log('find nextAssigned Id error'))
+
+    })
+
 }
 async function RunAccountsService(app){
     const uri="mongodb://localhost:27017/chatroom"
     const client=new MongoClient(uri)
     await client.connect()
     const db=client.db()
+
     const usersCollection=db.collection('users')
+    const nextAssignedIdCollection=db.collection('nextAssignedId')
+
     addLoginListener(app,usersCollection)
+    addRegisterListener(app,usersCollection,nextAssignedIdCollection)
+
 }
 
 module.exports=RunAccountsService
